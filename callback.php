a9a90b589f2344dd12deb135a9e20bfb9f686d7e
Mostafa <mostafazs@users.noreply.github.com>
2015-08-13 16:23:05 +0430

    first

---
+++ callback.php
@@ -0,0 +1,46 @@
+﻿<?php 
+	//callback(redirect) file
+	include_once('sender.php');
+// first here is better to get $result id ( or gateway-???) id and verify ir with id that gateway post back to this file($id_get)
+	
+	//not good method for geting saved value
+	$frh=fopen('value.txt','r') or die("Unable to open file!<br/>");
+	$our_save_id_get=fread($frh,filesize('value.txt'));
+	fclose($frh);
+
+	$url ='http://payline.ir/payment-test/gateway-result-second'; 
+	$api = 'adxcv-zzadq-polkjsad-opp13opoz-1sdf455aadzmck1244567'; 
+	$trans_id = $_POST['trans_id'];
+	$id_get = $_POST['id_get'];
+	if($our_save_id_get != $id_get){
+		echo "Error id_get not equal to our_save_id_get<br/>";
+	}
+	if($id_get > 0 && is_numeric($id_get)){ 
+	$result =get($url,$api,$trans_id,$id_get);
+	
+		switch($result){ 
+		case '-1' : 
+		echo 'API no correct'; 
+		break; 
+		case '-2' : 
+		echo 'trans_id not correct.'; 
+		break; 
+		case '-3' : 
+		echo 'id_get not correct.'; 
+		break; 
+		case '-4' : 
+		echo 'This transaction not exist , or not success.'; 
+		break; 
+		case '1' : 
+		echo ' transaction success.'; 
+		break;
+		}
+		echo "<hr/>";
+		var_dump($result);
+		echo "<hr/>";
+	}
+	else
+	{
+		echo "id_get not > 0 or numeric";
+	}
+	//here we must save all(in fact update) this info to database for future support of customer
---
+++ send.php
@@ -0,0 +1,26 @@
+﻿<?php 
+	//include our cURL functions
+	include_once("sender.php");
+
+
+
+//$url =' http://payline.ir/payment-test/gateway-76389';
+$url ='http://payline.ir/payment-test/gateway-send'; 
+$api_test = 'adxcv-zzadq-polkjsad-opp13opoz-1sdf455aadzmck1244567'; 
+$amount = 1000; 
+$redirect = urlencode('http://manasaman.host22.com/payline/callback.php');
+$result = send($url,$api_test,$amount,$redirect); 
+if($result > 0 && is_numeric($result)){ 
+//create a file for save $id_get value : this is nod good way : just for testing
+$fh=fopen("value.txt","w")  or die("Unable to open file!");
+$res=fwrite($fh,$result);
+fclose($fh);
+//here we save $result number no database , for verify it in page callback.php
+$go ="http://payline.ir/payment-test/gateway-$result";
+header("Location: $go"); 
+}
+else {
+	echo "Error";
+	echo "<br/>";
+	var_dump($result);
+}
